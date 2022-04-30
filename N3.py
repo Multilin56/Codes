8dc24b7329ae7b0b85d1f43de83506b4e1782703
Multilin56 <Miguely@mail.ru>
2022-04-30 15:20:49 +0300

    N3

--- N3.py
+++ N3.py
@@ -2,9 +2,9 @@ a = {1: "Yes", 2: "No", 3: "Maybe"}
 b = []
 
 for i in range(1, 4):
-    b.append(b)
+    b.append(len(a[i]))
 b = sorted(b)
-for o in range (3):
+for o in range(3):
     o = b[o]
     for p in range(1, 4):
         s = len(a[p])
