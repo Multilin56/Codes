360065d690d56994971af30697dee133f95ba9d2
Multilin56 <Miguely@mail.ru>
2022-08-06 19:15:16 +0300

    Create sidebar-admin.php

---
+++ PHP/app/include/sidebar-admin.php
@@ -0,0 +1,16 @@
+<?php include("../../assets/path.php"); ?>
+
+<div class="row">
+  <div class="sidebar col-3">
+    <ul>
+      <li>
+        <a href="<?php echo ADMIN_URL . "posts/"; ?>">Записи</a>
+      </li>
+      <li>
+        <a href="<?php echo ADMIN_URL . "topics/"; ?>">Категории</a>
+      </li>
+      <li>
+        <a href="<?php echo ADMIN_URL . "users/"; ?>">Пользователи</a>
+      </li>
+    </ul>
+  </div>
