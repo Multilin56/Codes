9e62229027a5ec834c70572f0eb6fc132476cd7e
Multilin56 <Miguely@mail.ru>
2022-08-06 19:14:10 +0300

    Create path.php

---
+++ PHP/assets/path.php
@@ -0,0 +1,6 @@
+<?php
+
+const SITE_ROOT = __DIR__;
+const BASE_URL = "http://localhost/Codes/PHP/assets/";
+const ADMIN_URL = "http://localhost/Codes/PHP/admin/";
+define("ROOT_PATH", realpath(dirname(__FILE__)));
