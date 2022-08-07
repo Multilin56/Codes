e28d1c71fdc6ed223927b55f70fed0ba73b98089
Multilin56 <Miguely@mail.ru>
2022-08-07 20:46:46 +0300

    Update index.php

--- PHP/assets/index.php
+++ PHP/assets/index.php
@@ -137,28 +137,9 @@
         </div>
 
         <!-- Блок-поиск -->
-        <div class="sidebar col-md-3 col-12">
-          <div class="section search">
-            <h3>Поиск</h3>
-            <form action="index.php" method="post">
-              <input
-                type="text"
-                name="search-ferm"
-                class="text-input"
-                placeholder="Введите искомое слово..."
-              />
-            </form>
-          </div>
+        
+        <?php include("../app/include/sidebar.php"); ?>
 
-          <div class="section topics">
-            <h3>Категории</h3>
-            <ul>
-              <?php foreach($topics as $key => $topic): ?>
-              <li><a href="#"><?=$topic['name']; ?></a></li>
-              <?php endforeach; ?>
-            </ul>
-          </div>
-        </div>
       </div>
     </div>
 
